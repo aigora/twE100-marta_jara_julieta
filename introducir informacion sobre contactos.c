f4c352f650b8bad54196426418cfab3e9ef427e7
julietaguerrerov <47822017+julietaguerrerov@users.noreply.github.com>
2019-04-27 12:59:15 +0200

    nombre, telefono, email

---
+++ introducir informacion sobre contactos.c
@@ -0,0 +1,15 @@
+// guardar contactos
+int main()
+{
+	char nombre [75], telefono [20], email [75];
+
+	printf ("NOMBRE Y APELLIDOS: ");
+	scanf ("%s", nombre);
+	printf ("\nTELEFONO: ");
+	scanf ("%s", telefono);
+	printf ("\nEMAIL: ");
+	scanf ("%s", email);
+	printf ("Se ha guardado %s como nuevo contacto", nombre);
+}
+
+
